84f25ed0b4dede1f4c34735a23127601e8d0d664
Igor Konovalov <konovalov.avp@gmail.com>
2017-01-24 23:37:04 +0300

    hexagon draw func beginning

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 console.log('hello');
 const cx = document.querySelector("canvas").getContext("2d");
 
+function hexCorner(center, size, i) { // in progress
+  var angle_deg = 60 * i;
+  var angle_rad = PI / 180 * angle_deg;
+}
+
 cx.beginPath();
 cx.rect(20, 20, 150, 150);
 cx.fillStyle = "red";
