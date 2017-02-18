d165fe8feedfbb8ddbcebf16d66185091093b3e4
Igor Konovalov <konovalov.avp@gmail.com>
2017-02-18 19:12:56 +0300

    svg mode on

--- Islamic_Star_Patterns_SVG/src/index.js
+++ Islamic_Star_Patterns_SVG/src/index.js
@@ -2,10 +2,10 @@ const canvas = document.getElementById('test');
 const cx = canvas.getContext("2d");
 
 let polyTest = new Polygon(4);
-polyTest.addVertex(100,100);
-polyTest.addVertex(200,100);
-polyTest.addVertex(200,200);
-polyTest.addVertex(100,200);
+polyTest.addVertex(50,50);
+polyTest.addVertex(150,50);
+polyTest.addVertex(150,150);
+polyTest.addVertex(50,150);
 polyTest.close();
 polyTest.hankin();
 polyTest.show();
--- Islamic_Star_Patterns_SVG/src/vector.js
+++ Islamic_Star_Patterns_SVG/src/vector.js
@@ -3,6 +3,7 @@ function Vector(x, y) {
   this.y = y;
   this.length = Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
 }
+
 Vector.prototype.length = function () {
   return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
 }
@@ -29,14 +30,6 @@ Vector.prototype.rotate = function(angle) {
     x = Math.round(10000*(this.x * Math.cos(angle) - this.y * Math.sin(angle)))/10000;
     y = Math.round(10000*(this.x * Math.sin(angle) + this.y * Math.cos(angle)))/10000;
     return new Vector(x, y);
-}
-
-Vector.prototype.middle = function(end) {
-  let line = Math.sqrt(Math.pow((this.x - end.x), 2) + Math.pow((this.y - end.y),2));
-  let middleLength = line / 2;
-  x = this.x - (middleLength * (this.x - end.x)) / line;
-  y = this.y - (middleLength * (this.y - end.y)) / line;
-  return new Vector(x, y);
 }
 
 Vector.prototype.setMagnitude = function(magnitude) {
