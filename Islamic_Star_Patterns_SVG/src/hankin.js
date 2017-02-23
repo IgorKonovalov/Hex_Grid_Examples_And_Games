2b47fde73d4e517c6ee04f04af40aea8ff978b1e
Igor Konovalov <konovalov.avp@gmail.com>
2017-02-23 21:27:47 +0300

    minor tweaks

--- Islamic_Star_Patterns_SVG/src/hankin.js
+++ Islamic_Star_Patterns_SVG/src/hankin.js
@@ -4,10 +4,11 @@ function Hankin(a, v) {
   this.end = a.plus(v);
   this.coord = [this.a, this.end];
 
+
   this.show = function() {
     cx.beginPath();
     cx.lineWidth = 1.5;
-    cx.strokeStyle = "violet";
+    cx.strokeStyle = "magenta";
     cx.moveTo(this.a.x, this.a.y);
     cx.lineTo(this.end.x, this.end.y);
     cx.stroke();
